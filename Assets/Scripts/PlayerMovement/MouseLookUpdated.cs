396a7a9157fcf34d0b1851e38a1cfb98be4e9174
Malteleh <148697349+Malteleh@users.noreply.github.com>
2024-05-03 18:27:02 +0200

    Guard

--- Assets/Scripts/PlayerMovement/MouseLookUpdated.cs
+++ Assets/Scripts/PlayerMovement/MouseLookUpdated.cs
@@ -1,23 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLookUpdated : MonoBehaviour
 {
-    
-
     public float mouseSensitivity = 100f;
-
     public Transform playerBody;
-
+    
     float xRotation = 0f;
-    public Vector2 sensitivity = Vector2.one * 360f;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
 
     void Update()
     {
@@ -31,3 +24,4 @@ public class MouseLookUpdated : MonoBehaviour
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
+
